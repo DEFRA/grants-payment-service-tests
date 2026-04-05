7e6f19b443999aedeaeef0952359a255ee207c4d
Arijit Ghosh <arijit.ghosh@equalexperts.com>
2026-04-06 00:01:33 +0100

    GPS Automation Test

--- wdio.conf.js
+++ wdio.conf.js
@@ -14,8 +14,7 @@ export const config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: `https://ephemeral-protected.api.${process.env.ENVIRONMENT}.cdp-int.defra.cloud/grants-payment-service/`,
-
+  baseUrl: `https://grants-payment-service.${process.env.ENVIRONMENT}.cdp-int.defra.cloud/`,
   // Connection to remote chromedriver
   hostname: process.env.CHROMEDRIVER_URL || '127.0.0.1',
   port: process.env.CHROMEDRIVER_PORT || 4444,
